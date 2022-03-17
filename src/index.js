9dccdba45db1b912ba52583c28711c5a40cd9bdf
Fernando Cruz <64991242+dcruz1995@users.noreply.github.com>
2022-03-17 15:23:28 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//atv 01
+//Fernando
+var name = "Fernando";
+var year = 27;
+var thisYear = 2022;
+var quartaFeira = false;
+console.log(thisYear - year);
+console.log("Estou mexendo no console.log");
+console.log(quartaFeira);
+console.log(typeof quartaFeira);
